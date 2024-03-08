eaf11526534afd09200512fe25825d0b1719efc4
guhpissai <gustavopissai3d@gmail.com>
2024-03-08 09:41:04 -0300

    Implementando a classe Estatisticas e adicionando o valor total ao document

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,4 @@
+import Estatisticas from "./Estatisticas.js";
 import fetchData from "./fetchData.js";
 import normalizarTransacao from "./normalizarTransacao.js";
 
@@ -8,6 +9,17 @@ async function handleData() {
   if (!data) return;
   const transacoes = data.map(normalizarTransacao);
   preencherTabela(transacoes);
+  preencherEstatisticas(transacoes);
+}
+
+function preencherEstatisticas(transacoes: Transacao[]): void {
+  const data = new Estatisticas(transacoes);
+  const estatistica = document.querySelector<HTMLElement>("#total span");
+  if (!estatistica) return;
+  estatistica.innerText = data.total.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 }
 
 function preencherTabela(transacoes: Transacao[]): void {
